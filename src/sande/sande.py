6205a3f2b7fb54c28b2457060b4351c31f005da3
Clint Daniels <clint.daniels@sandag.org>
2017-05-08 13:14:02 -0700

    First code import.

---
+++ src/sande/sande.py
@@ -0,0 +1,11 @@
+# SANDAG SanDSim
+# See full license in LICENSE.txt
+
+import sys
+
+def main():
+    """Main entry point for the application"""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
